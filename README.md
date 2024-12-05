72a37600605cf7f4643efd3bb351be676f978bfa
Fredrik Teschke <f@ftes.de>
2024-12-05 20:01:13 +0100

    Set up mix.exs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PhoenixTestPlaywright
 
-**TODO: Add description**
+A Playwright driver that executes your PhoenixTest cases in an actual browser.
 
 ## Installation
 
@@ -18,4 +18,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/phoenix_test_playwright>.
-
--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule PhoenixTestPlaywright.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/ftes/phoenix_test_playwright"
+  @description """
+  A Playwright driver that executes your PhoenixTest cases in an actual browser.
+  """
+
   def project do
     [
       app: :phoenix_test_playwright,
-      version: "0.1.0",
-      elixir: "~> 1.17",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: @version,
+      description: @description,
+      elixir: "~> 1.15",
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
+      name: "PhoenixTestPlaywright",
+      source_url: @source_url,
+      docs: docs(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +33,33 @@ defmodule PhoenixTestPlaywright.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_test, "~> 0.4.2", only: :test, runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"Github" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "PhoenixTest",
+      extras: [
+        "CHANGELOG.md": [title: "Changelog"],
+        "upgrade_guides.md": [title: "Upgrade Guides"]
+      ]
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"]
     ]
   end
 end
