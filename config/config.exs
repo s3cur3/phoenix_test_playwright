52e002c0fd6c30ea1b27bee41d166bb037dae102
Fredrik Teschke <f@ftes.de>
2025-11-14 08:43:56 +0100

    Move repo, fix config for 'mix docs'

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,5 @@
 import Config
 
-config :phoenix_test_playwright, PhoenixTest.Playwright.Repo,
-  database: "phoenix_test_playwright_repo",
-  username: "user",
-  password: "pass",
-  hostname: "localhost"
+config :phoenix_test, endpoint: PhoenixTest.Endpoint
 
 import_config "#{config_env()}.exs"
