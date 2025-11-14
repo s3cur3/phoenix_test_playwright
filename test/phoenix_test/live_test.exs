4743880c74d613b0578cb22e2999b82983417630
Fredrik Teschke <f@ftes.de>
2025-11-14 06:07:20 +0100

    Categorize failing upstream PhoenixTest tests and run with Playwright.Case

--- test/phoenix_test/live_test.exs
+++ test/phoenix_test/live_test.exs
@@ -4,6 +4,7 @@ defmodule PhoenixTest.LiveTest do
   alias ExUnit.AssertionError
   alias PhoenixTest.Driver
   alias PhoenixTest.Html
+  alias PhoenixTest.Playwright.Case
 
   describe "render_page_title/1" do
     @tag skip: "investigate"
